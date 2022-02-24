7ba163720fb78d02535e50b046a31c9558639077
easydush <izidacasillas@mail.ru>
2022-02-24 13:17:00 +0300

    added tokenizer

--- easy/easy/spiders/common.py
+++ easy/easy/spiders/common.py
@@ -1,10 +1,11 @@
+import lxml
 import scrapy
 import logging
 
 
 class CommonSpider(scrapy.Spider):
     name = 'common'
-    start_urls = ['https://moz.com/top500']
+    start_urls = ['https://vc.ru/flood/170561-chto-delat-esli-skuchno-500-ssylok-sobrannyh-za-polgoda']
 
     def parse(self, response):
         urls = [link for link in response.selector.xpath('//a/@href').getall() if
@@ -18,7 +19,9 @@ class CommonSpider(scrapy.Spider):
         with open('../index.txt', 'a') as f:
             f.write(f'{num} {response.url} \n')
             f.close()
-        filename = f"../pages/{num}.html"
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        filename = f"../pages/{num}.txt"
+        tree = lxml.html.fromstring(response.body)
+        all_text = tree.text_content().strip()
+        with open(filename, 'w', encoding='UTF-8') as f:
+            f.write(all_text)
         logging.info(f'Saved file {filename}')
---
+++ tokens/tokenizer.py
@@ -0,0 +1,57 @@
+import os.path
+
+import nltk
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+NLTK_PACKAGES = ['tokenizers/punkt', 'corpora/stopwords', 'corpora/wordnet', 'corpora/omw-1.4']
+STOPWORDS = stopwords.words('english')
+
+
+def tokenize(text):
+    tokens = []
+
+    tokens += nltk.word_tokenize(text)
+    lowered_tokens = [token.lower() for token in tokens]
+    return [item for item in lowered_tokens if item not in STOPWORDS]
+
+
+def lemmatize(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemma_token = dict()
+
+    for token in tokens:
+        lemmatized = lemmatizer.lemmatize(token)
+
+        mapped_tokens = lemma_token.get(lemmatized, [])
+        mapped_tokens.append(token)
+
+        lemma_token[lemmatized] = mapped_tokens
+    return lemma_token
+
+
+def scan_nltk_packages():
+    for package in NLTK_PACKAGES:
+        try:
+            nltk.find(package)
+        except Exception:
+            nltk.download(package.split('/')[1])
+
+
+if __name__ == '__main__':
+    scan_nltk_packages()
+
+    tokens = set()
+    for page in os.listdir('../pages'):
+        with open(os.path.join('../pages', page), encoding='UTF-8') as f:
+            tokens.update(set(tokenize(f.read())))
+
+    tokens = set(tokens)
+
+    with open('./tokens.txt', mode='wt', encoding='utf-8') as file:
+        file.write('\n'.join(tokens))
+
+    lemma_token = lemmatize(tokens)
+    with open('./lemmas.txt', mode='at', encoding='utf-8') as file:
+        for key, value in lemma_token.items():
+            file.write(f'{key}: {value}\n')
